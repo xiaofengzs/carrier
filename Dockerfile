17533b0ad0a9da83e8c80ac959102b8b0e6cb521
Xiaofeng Zhi <xiaofengzhi@XiaofengdeMacBook-Pro.local>
2022-12-04 08:10:56 +0800

    Xiaofeng: Modify Dockerfile.

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,28 @@
-FROM alpine:latest
+FROM golang:1.18.3-alpine3.16 AS build_base
 
 WORKDIR /app
-COPY ./controller /app/controller
-#RUN chmod 550 /app/controller
 
-CMD ["/controller"]
+ENV GO111MODULE=on
+ENV GOPROXY=https://goproxy.cn,direct
+
+COPY go.mod ./
+COPY go.sum ./
+
+RUN go mod download
+
+COPY . .
+
+RUN go build -o  carrier-controller  ./
+
+# Start fresh from a smaller image
+FROM alpine:3.16
+
+WORKDIR /app
+
+COPY --from=build_base /app/carrier-controller  /app/carrier-controller
+
+# This container exposes port 8080 to the outside world
+EXPOSE 8080
+
+# Run the binary program produced by `go install`
+CMD ["./carrier-controller"]
--- main.go
+++ main.go
@@ -17,12 +17,19 @@ limitations under the License.
 package main
 
 import (
+	clientset "carrier/pkg/generated/clientset/versioned"
+	informers "carrier/pkg/generated/informers/externalversions"
 	"flag"
 	"fmt"
+	kubeinformers "k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/klog/v2"
+	"k8s.io/sample-controller/pkg/signals"
+	"time"
 
 	// Uncomment the following line to load the gcp plugin (only required to authenticate against GKE clusters).
 	// _ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
-
 )
 
 var (
@@ -33,44 +40,44 @@ var (
 func main() {
 
 	fmt.Println("test my controller")
-	//klog.InitFlags(nil)
-	//flag.Parse()
-	//
-	//klog.Infof("Start serving for carrier.")
-	//
-	//// set up signals so we handle the first shutdown signal gracefully
-	//stopCh := signals.SetupSignalHandler()
-	//
-	//cfg, err := clientcmd.BuildConfigFromFlags(masterURL, kubeconfig)
-	//if err != nil {
-	//	klog.Fatalf("Error building kubeconfig: %s", err.Error())
-	//}
-	//
-	//kubeClient, err := kubernetes.NewForConfig(cfg)
-	//if err != nil {
-	//	klog.Fatalf("Error building kubernetes clientset: %s", err.Error())
-	//}
-	//
-	//carrierClient, err := clientset.NewForConfig(cfg)
-	//if err != nil {
-	//	klog.Fatalf("Error building carrier clientset: %s", err.Error())
-	//}
-	//
-	//kubeInformerFactory := kubeinformers.NewSharedInformerFactory(kubeClient, time.Second*30)
-	//carrierInformerFactory := informers.NewSharedInformerFactory(carrierClient, time.Second*30)
-	//
-	//controller := NewController(kubeClient, carrierClient,
-	//	kubeInformerFactory.Apps().V1().Deployments(),
-	//	carrierInformerFactory.Carrier().V1().Carriers())
-	//
-	//// notice that there is no need to run Start methods in a separate goroutine. (i.e. go kubeInformerFactory.Start(stopCh)
-	//// Start method is non-blocking and runs all registered informers in a dedicated goroutine.
-	//kubeInformerFactory.Start(stopCh)
-	//carrierInformerFactory.Start(stopCh)
-	//
-	//if err = controller.Run(2, stopCh); err != nil {
-	//	klog.Fatalf("Error running controller: %s", err.Error())
-	//}
+	klog.InitFlags(nil)
+	flag.Parse()
+
+	klog.Infof("Start serving for carrier.")
+
+	// set up signals so we handle the first shutdown signal gracefully
+	stopCh := signals.SetupSignalHandler()
+
+	cfg, err := clientcmd.BuildConfigFromFlags(masterURL, kubeconfig)
+	if err != nil {
+		klog.Fatalf("Error building kubeconfig: %s", err.Error())
+	}
+
+	kubeClient, err := kubernetes.NewForConfig(cfg)
+	if err != nil {
+		klog.Fatalf("Error building kubernetes clientset: %s", err.Error())
+	}
+
+	carrierClient, err := clientset.NewForConfig(cfg)
+	if err != nil {
+		klog.Fatalf("Error building carrier clientset: %s", err.Error())
+	}
+
+	kubeInformerFactory := kubeinformers.NewSharedInformerFactory(kubeClient, time.Second*30)
+	carrierInformerFactory := informers.NewSharedInformerFactory(carrierClient, time.Second*30)
+
+	controller := NewController(kubeClient, carrierClient,
+		kubeInformerFactory.Apps().V1().Deployments(),
+		carrierInformerFactory.Carrier().V1().Carriers())
+
+	// notice that there is no need to run Start methods in a separate goroutine. (i.e. go kubeInformerFactory.Start(stopCh)
+	// Start method is non-blocking and runs all registered informers in a dedicated goroutine.
+	kubeInformerFactory.Start(stopCh)
+	carrierInformerFactory.Start(stopCh)
+
+	if err = controller.Run(2, stopCh); err != nil {
+		klog.Fatalf("Error running controller: %s", err.Error())
+	}
 }
 
 func init() {
